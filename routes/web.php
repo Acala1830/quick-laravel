d8db5495ae472104d0a527a35eeef8a9e152dea4
Willosa <willosa1830@gmail.com>
2025-05-17 18:59:39 +0900

    Part5 完了

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\HelloController;
 use \App\Http\Controllers\ViewController;
+use \App\Http\Controllers\RouteController;
 // 「/」に対するルート
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +12,9 @@ Route::get('/', function () {
 // Route::get('/hello', [HelloController::class, 'index']);
 Route::get('/hello/view', [HelloController::class, 'view']);
 Route::get('/hello/list', [HelloController::class, 'list']);
+// Route::controller([HelloController::class]) -> group(function () {
+//     Route:get('hello/view', 'view');
+// });
 Route::get('/view/escape', [ViewController::class, 'escape']);
 Route::get('/view/if', [ViewController::class, 'if']);
 Route::get('/view/isset', [ViewController::class, 'isset']);
@@ -24,6 +28,13 @@ Route::get('/view/checked', [ViewController::class, 'checked']);
 Route::get('/view/master', [ViewController::class, 'master']);
 Route::get('/view/comp', [ViewController::class, 'comp']);
 Route::get('/view/list', [ViewController::class, 'list']);
+Route::get('/route/param/{id?}', [RouteController::class, 'param']) -> whereNumber('id');
+Route::get('/route/search/{keywd?}', [RouteController::class, 'search']) -> where('keywd', '.*');
+Route::get('/route/enum_param/{category}', [RouteController::class, 'enum_param']);
+Route::redirect('/hoge','/', 301);
+Route::fallback(function() {
+    return view('route.error');
+});
 // 4.5.2~
 // Route::get('/view/forelse', [ViewController::class, 'forelse']);
 // Route::get('/hello/view', 'HelloController@view');
